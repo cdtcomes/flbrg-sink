3b8b18c071a610e4c1b544fa21d2bf4cf9910ea4
Sergio Schuster Muñoz <sschuster@cabledatatelecom.com>
2025-07-21 17:40:16 +0200

    Añadir opción para redirigir home

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+const config = useRuntimeConfig()
+const homeURL = config.public.homeURL
+
+if (import.meta.client && homeURL) {
+  navigateTo(homeURL, { external: true })
+}
+</script>
+
 <template>
   <main class="flex flex-col justify-center">
     <HomeTwitter />
